2748ba6aba019c562b1c47ef073baca0156444f4
prafpawar11 <prafpawar11@gmail.com>
2026-03-27 20:31:46 +0530

    Login TC by Mayur

---
+++ src/test/java/testLayer/Cogmento.java
@@ -0,0 +1,36 @@
+package testLayer;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Cogmento {
+	static WebDriver driver;
+
+	@BeforeTest
+	public void openaUrl() {
+		driver = new ChromeDriver();
+		driver.get("https://ui.cogmento.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	}
+
+	@Test
+	public void loginFunctionality() {
+		driver.findElement(By.name("email")).sendKeys("prafulp1010@gmail.com");
+		driver.findElement(By.name("password")).sendKeys("Pr@ful0812");
+		driver.findElement(By.xpath("//div[text()='Login']")).click();
+	}
+
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
